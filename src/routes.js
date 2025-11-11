b66db879fc73cea09d339252ff35bf0c7bdfaae4
mhndkptr <muhamadhendika77@gmail.com>
2025-11-11 15:35:13 +0700

    chore: add build query options

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,15 @@ import express from "express";
 import authRoutes from "./domains/v1/auth/auth-routes.js";
 import courseRoutes from "./domains/v1/course/course-routes.js";
 import scheduleRoutes from "./domains/v1/schedule/schedule-routes.js";
+import quizRoutes from "./domains/v1/quiz/quiz-routes.js";
 
 const router = express.Router();
 
 const appsV1Routes = [
   { path: "/auth", route: authRoutes },
   { path: "/course", route: courseRoutes },
-  { path: "/schedule", route: scheduleRoutes }, 
+  { path: "/schedule", route: scheduleRoutes },
+  { path: "/quiz", route: quizRoutes },
 ];
 
 appsV1Routes.forEach(({ path, route }) => {

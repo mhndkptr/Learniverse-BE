9a2e869f07b47347bc3f2f943e0f1183eaa30fda
mhndkptr <muhamadhendika77@gmail.com>
2025-11-21 04:53:32 +0700

    fix: remove upload file static

--- src/domains/v1/user/user-routes.js
+++ src/domains/v1/user/user-routes.js
@@ -1,6 +1,9 @@
 import BaseRoutes from "../../../base-classes/base-routes.js";
-import multer from "multer";
-import { createUserSchema, updateUserSchema, getAllUserParamsSchema} from "./user.schema.js";
+import {
+  createUserSchema,
+  updateUserSchema,
+  getAllUserParamsSchema,
+} from "./user.schema.js";
 import validateCredentials from "../../../middlewares/validate-credentials-middleware.js";
 import authTokenMiddleware from "../../../middlewares/auth-token-middleware.js";
 import tryCatch from "../../../utils/tryCatcher.js";
@@ -8,48 +11,43 @@ import userController from "./user-controller.js";
 import Role from "../../../common/enums/role-enum.js";
 import validateQueryParamsCredentials from "../../../middlewares/validate-query-params-credentials-middleware.js";
 
-
-const upload = multer({ dest: "uploads/profiles/" });
-
 class UserRoutes extends BaseRoutes {
-	routes() {
-		this.router.get("/", [
-			authTokenMiddleware.authenticate, 
-			authTokenMiddleware.authorizeRoles([Role.ADMIN]),
-			validateQueryParamsCredentials(getAllUserParamsSchema),
-			tryCatch(userController.getAll),
-		]);
-		this.router.get("/:id", [
-			authTokenMiddleware.authenticate, 
-			authTokenMiddleware.authorizeRoles([Role.ADMIN]),
-			tryCatch(userController.getById),
-		]);
+  routes() {
+    this.router.get("/", [
+      authTokenMiddleware.authenticate,
+      authTokenMiddleware.authorizeRoles([Role.ADMIN]),
+      validateQueryParamsCredentials(getAllUserParamsSchema),
+      tryCatch(userController.getAll),
+    ]);
+    this.router.get("/:id", [
+      authTokenMiddleware.authenticate,
+      authTokenMiddleware.authorizeRoles([Role.ADMIN]),
+      tryCatch(userController.getById),
+    ]);
 
-		this.router.post(
-			"/", 
-			authTokenMiddleware.authenticate,
-			authTokenMiddleware.authorizeRoles([Role.ADMIN]),
-			validateCredentials(createUserSchema),
-			tryCatch(userController.create)
-		);
+    this.router.post(
+      "/",
+      authTokenMiddleware.authenticate,
+      authTokenMiddleware.authorizeRoles([Role.ADMIN]),
+      validateCredentials(createUserSchema),
+      tryCatch(userController.create)
+    );
 
-		this.router.patch(
-			"/:id",
-			authTokenMiddleware.authenticate,
-			authTokenMiddleware.authorizeRoles([Role.ADMIN]),
-			validateCredentials(updateUserSchema),
-			tryCatch(userController.update)
-		);
+    this.router.patch(
+      "/:id",
+      authTokenMiddleware.authenticate,
+      authTokenMiddleware.authorizeRoles([Role.ADMIN]),
+      validateCredentials(updateUserSchema),
+      tryCatch(userController.update)
+    );
 
-		this.router.delete(
-			"/:id",
-			authTokenMiddleware.authenticate,
-			authTokenMiddleware.authorizeRoles([Role.ADMIN]),
-			tryCatch(userController.remove)
-		);
-
-	}
+    this.router.delete(
+      "/:id",
+      authTokenMiddleware.authenticate,
+      authTokenMiddleware.authorizeRoles([Role.ADMIN]),
+      tryCatch(userController.remove)
+    );
+  }
 }
 
 export default new UserRoutes().router;
-

3123d1b440dc642347831876ef506450295d73a0
Adaannn <adann@MacBook-Air-Muhammad.local>
2025-12-07 21:56:44 +0700

    Reviced API Course

--- src/domains/v1/course/course-controller.js
+++ src/domains/v1/course/course-controller.js
@@ -22,14 +22,44 @@ class CourseController {
   async create(req, res) {
     if (!req.body) throw BaseError.badRequest("Request body is missing");
     if (!req.file) throw BaseError.badRequest("Image file is missing");
-    const data = await CourseService.create(req.body, req.user, req.file);
+
+    const payload = { ...req.body };
+
+    if (payload.price) {
+      payload.price = parseFloat(payload.price);
+    }
+
+    payload.is_open_registration_member =
+      String(payload.is_open_registration_member) === "true";
+
+    payload.is_open_registration_mentor =
+      String(payload.is_open_registration_mentor) === "true";
+
+    const data = await CourseService.create(payload, req.user, req.file);
     return createdResponse(res, data, "Course created successfully");
   }
 
   async update(req, res) {
     const { id } = req.params;
     if (!req.body) throw BaseError.badRequest("Request body is missing");
-    const data = await CourseService.update(id, req.body, req.user, req.file);
+
+    const payload = { ...req.body };
+
+    if (payload.price) {
+      payload.price = parseFloat(payload.price);
+    }
+
+    if (payload.is_open_registration_member !== undefined) {
+      payload.is_open_registration_member =
+        String(payload.is_open_registration_member) === "true";
+    }
+
+    if (payload.is_open_registration_mentor !== undefined) {
+      payload.is_open_registration_mentor =
+        String(payload.is_open_registration_mentor) === "true";
+    }
+
+    const data = await CourseService.update(id, payload, req.user, req.file);
     return successResponse(res, data, "Course updated successfully");
   }
 
